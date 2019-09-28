3a278618208c8f431c85fb1c2c0cf29a1d03e267
Chuck Rai <cprai@sfu.ca>
2019-09-28 14:20:41 -0700

    Vector3 class

---
+++ renderer/src/geometry/vector3.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace renderer {
+
+
+class Vector3 {
+public:
+    float x;
+    float y;
+    float z;
+
+    Vector3(float x, float y, float z) : x(x), y(y), z(z) { }
+
+};
+
+
+}
