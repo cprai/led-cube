724ddc1d7e99b80b5bcadddb708d90597682f66f
Chuck Rai <cprai@sfu.ca>
2019-09-28 16:52:33 -0700

    Default constructors for Vertex and Vector3

--- renderer/src/geometry/vector3.h
+++ renderer/src/geometry/vector3.h
@@ -9,6 +9,7 @@ public:
     float y;
     float z;
 
+    Vector3() : x(0.0f), y(0.0f), z(0.0f) { }
     Vector3(float x, float y, float z) : x(x), y(y), z(z) { }
 
 };
--- renderer/src/geometry/vertex.h
+++ renderer/src/geometry/vertex.h
@@ -9,6 +9,7 @@ public:
     float y;
     float z;
 
+    Vertex() : x(0.0f), y(0.0f), z(0.0f) { }
     Vertex(float x, float y, float z) : x(x), y(y), z(z) { }
 
 };
