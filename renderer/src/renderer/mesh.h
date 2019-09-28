8b5b3ffe3bc7dacc6f0f213d3e60fdea96df940f
Chuck Rai <cprai@sfu.ca>
2019-09-28 14:24:16 -0700

    Mesh class

---
+++ renderer/src/renderer/mesh.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+#include "geometry/vertex.h"
+#include "geometry/triangle.h"
+
+namespace renderer {
+
+class Mesh {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<Triangle> triangles;
+}
+
+
+}
