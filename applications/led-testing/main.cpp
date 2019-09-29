2c1f1209aec9cad09cdb1d9c31aa5de85688cd94
Chuck Rai <cprai@sfu.ca>
2019-09-29 12:28:55 -0700

    Simple driver for testing LED display code

---
+++ applications/led-testing/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "geometry/vector3.h"
+#include "displayer/displayer.h"
+
+#define N 5
+
+int main() {
+    displayer::Displayer displayer;
+
+    renderer::Vector3 outputBuffer[N];
+    for (int i = 0; i < N; i += 2) {
+        outputBuffer[i] = {1.0f, 0.0f, 0.0f};
+    }
+
+    std::cout << "Output buffer:" << std::endl;
+    for (int i = 0; i < N; i++) {
+        std::cout << "{" << outputBuffer[i].x << "," << outputBuffer[i].y << "," << outputBuffer[i].z << "}" << std::endl;
+    }
+
+    displayer.display<N>((displayer::Color*)outputBuffer);
+}
