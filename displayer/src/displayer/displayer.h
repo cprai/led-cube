b361524551bfbc1d889f0a7ceddcdfec193c161e
Chuck Rai <cprai@sfu.ca>
2019-09-29 12:28:13 -0700

    Display function dumps input to stdout

--- displayer/src/displayer/displayer.h
+++ displayer/src/displayer/displayer.h
@@ -14,6 +14,11 @@ public:
     template<int LEDCount>
     void display(Color outputBuffer[LEDCount]) {
         std::cout << "Call to display module" << std::endl;
+
+        std::cout << "Recieved output buffer:" << std::endl;
+        for (int i = 0; i < LEDCount; i++) {
+            std::cout << "{" << outputBuffer[i].r << "," << outputBuffer[i].g << "," << outputBuffer[i].b << "}" << std::endl;
+        }
     }
 };
 
