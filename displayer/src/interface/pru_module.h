d5e464533057d4a661dbc3203d552b2fe9f9f4e1
Chuck Rai <cprai@sfu.ca>
2019-10-24 21:07:39 -0700

    Slight fixes to interface header file

--- displayer/src/interface/pru_module.h
+++ displayer/src/interface/pru_module.h
@@ -1,4 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace displayer {
+
+
 struct pru_module_t;
 extern "C" pru_module_t* pru_module_new(uint32_t num_leds);
 extern "C" void pru_module_update(pru_module_t* pru_module, uint32_t* new_data);
 extern "C" void pru_module_shutdown(pru_module_t* pru_module);
+
+
+}
