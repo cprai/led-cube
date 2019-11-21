52bd534750ff17926bf6a60bc0f100723cc36f19
Chuck Rai <cprai@sfu.ca>
2019-11-20 16:32:18 -0800

    Make joystick.h compile properly

--- controller/src/controller/joystick.h
+++ controller/src/controller/joystick.h
@@ -1,11 +1,8 @@
-#ifndef JOYSTICK_H_
-#define JOYSTICK_H_
-
-enum JSEvent { AXIS, BUTTON } joystick_event;
+enum JSEvent { AXIS, BUTTON };
 enum JSAxes { L_STICK_X, L_STICK_Y, L_TRIGGER, R_STICK_X, 
-                R_STICK_Y, R_TRIGGER, DPAD_X, DPAD_Y } Joystick_axis;
+                R_STICK_Y, R_TRIGGER, DPAD_X, DPAD_Y };
 enum JSButtons { A, B, X, Y ,L_BUMPER, R_BUMPER, SELECT, 
-                START, HOME, L_STICK, R_STICK } Joystick_button;
+                START, HOME, L_STICK, R_STICK };
 
 extern void Joystick_init(void);
 extern void Joystick_cleanUp(void);
@@ -13,5 +10,3 @@ extern void Joystick_cleanUp(void);
 extern int Joystick_getState(int, int);
 
 extern void Joystick_stop(void);
-
-#endif
